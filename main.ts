f889574a4783e32036976000b4227c7082ab7703
MarekKnobloch <marek.knobloch.024@pslib.cz>
2025-05-22 09:14:06 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ basic.forever(function () {
     let x = input.acceleration(Dimension.X)
     let y = input.acceleration(Dimension.Y)
 
-    // Odesíláme hodnoty jako jediný řetězec "x:y"
+    
     radio.sendString(x + ":" + y)
 
     basic.pause(50)
